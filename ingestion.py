955694926c285f305e7b46d1b1692e434f378159
savyata37 <savyata37@users.noreply.github.com>
2026-03-13 12:20:35 +0545

    abc

--- ingestion.py
+++ ingestion.py
@@ -1,43 +1,4 @@
-# import pandas as pd
-# # import yfinance as yf
-# import logging
 
-# logging.basicConfig(
-#     filename="pipeline.log",
-#     level=logging.INFO,
-#     format="%(asctime)s - %(levelname)s - %(message)s"
-# )
-
-
-# # def fetch_api_data(ticker="^GSPC", start="2015-01-01", end="2025-01-01"):
-# #     """
-# #     Fetch stock data from Yahoo Finance API
-# #     """
-# #     try:
-# #         df = yf.download(ticker, start=start, end=end)
-# #         df.reset_index(inplace=True)
-# #         logging.info("API data fetched successfully")
-# #         return df
-# #     except Exception as e:
-# #         logging.error("API fetch failed: %s", e)
-
-
-# def load_csv_chunked(file_path, chunk_size=10000):
-#     """
-#     Load large CSV files using chunking
-#     """
-#     chunks = []
-
-#     try:
-#         for chunk in pd.read_csv(file_path, chunksize=chunk_size):
-#             chunks.append(chunk)
-
-#         df = pd.concat(chunks, ignore_index=True)
-#         logging.info("CSV loaded with chunking")
-#         return df
-
-#     except Exception as e:
-#         logging.error("CSV loading failed: %s", e)
 
 import pandas as pd
 import logging
